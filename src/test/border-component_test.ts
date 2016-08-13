cce70c70aabeb4c87eb8e3ec5d8365aaaba65a34
jemae092312 <jemae092312>
2016-08-13 17:29:13 +0800

    this is a unit test subj NUM#1 function test with jasmin

---
+++ src/test/border-component_test.ts
@@ -0,0 +1,51 @@
+import {
+  async,
+  inject,
+  TestBed,
+  ComponentFixture
+} from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { BorderComponent } from '../app/border-component';
+
+@Component({
+  template: '',
+  directives: [BorderComponent]
+})
+class TestComponent {
+}
+
+describe('greeting component', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent]
+    });
+  });
+
+  it('should wrap content', async(() => {
+    TestBed.overrideComponent(TestComponent, {set: {
+      template: '<my-fancy-border>Content</my-fancy-border>'
+    }});
+
+    TestBed.compileComponents().then(() => {
+      var fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+      var compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled).toContainText('Content');
+    });
+  }));
+
+  it('should wrap content', async(() => {
+    TestBed.overrideComponent(TestComponent, {set: {
+      template: '<my-fancy-border title="ABC"></my-fancy-border>'
+    }});
+
+    TestBed.compileComponents().then(() => {
+      var fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+      var compiled = fixture.debugElement.nativeElement;
+
+      expect(compiled).toContainText('ABC');
+    });
+  }));
+});
